3b42cb5bccb9190c539206bdbbedd2f51e46844e
JoselynDRF <mariajoselyndosramos@yahoo.es>
2024-08-17 15:59:03 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="50" height="50" />
+    </header>
+
+    <main>
+      <element-form />
+    </main>
+  </div>
+</template>
+
+<script>
+import ElementForm from './components/ElementForm.vue'
+
+export default {
+  name: 'App',
+  components: { ElementForm }
+}
+</script>
